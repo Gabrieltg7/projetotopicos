53e8f278c925a0105a33822ba619f9fbead79f91
Gabriel Teixeira <gabrieltg777@gmail.com>
2022-09-27 03:33:10 +0000

    updateSecurity

--- src/main/java/br/gov/sp/fatec/projetotopicos/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/projetotopicos/entity/Usuario.java
@@ -1,14 +1,17 @@
 package br.gov.sp.fatec.projetotopicos.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.Set;
 
@@ -16,48 +19,68 @@ import javax.persistence.Column;
 
 @Entity
 @Table(name = "usr_usuario")
-
 public class Usuario {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "usr_id")
-	private Long id;
 
-	@Column(name = "usr_nome")
-	private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usr_id")
+    private Long id;
 
-	@Column(name = "usr_senha")
-	private String senha;
-	
-	@ManyToMany(mappedBy = "autozizacoes")
-	@JoinTable(name = "uau_usuario_autorizacao",
-	joinColumns = { @JoinColumn(name = "usr_id")},
-	inverseJoinColumns = { @JoinColumn(name = "aut_id")} )
-	private Set<Autorizacao> autorizacoes;
+    @Column(name = "usr_nome")
+    private String nome;
 
-	public Long getId() {
-		return id;		
-	}
+    @Column(name = "usr_senha")
+    private String senha;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = { @JoinColumn(name = "usr_id")},
+        inverseJoinColumns = { @JoinColumn(name = "aut_id") }
+        )
+    private Set<Autorizacao> autorizacoes;
 
-	public String getNome() {
-		return nome;
-	}
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario")
+    private Set<Anotacao> anotacoes;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Autorizacao> getAutorizacoes() {
+        return autorizacoes;
+    }
+
+    public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
+        this.autorizacoes = autorizacoes;
+    }
+
+    public Set<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(Set<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+    
 }
